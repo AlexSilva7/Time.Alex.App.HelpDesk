2b3fbfcd8f0178a0cc2f10c2a7c320d34339ffb4
Alex Silva <alex.silva@intelitrader.com.br>
2022-08-15 18:23:45 -0300

    ajusta erro no login

--- js/login.js
+++ js/login.js
@@ -1,26 +1,20 @@
 
 var admin = {
     username: "admin",
-    senha: "1234"
+    password: "1234"
 }
 
-
-function Login(){
+function Entrar(){
 
     var login = document.getElementById("login").value
     var senha = document.getElementById("password").value 
-    //window.location.href = "home.html";
-    if(login == admin.username){
-        alert(login)
+
+    if(login == admin.username && senha == admin.password){
+        alert("Logado com sucesso!");
         localStorage.setItem("Usuario", login);
         window.location.href = "home.html"; 
+    }else{
+        alert("Login ou senha incorretos!");
     }
-   /* if(login == admin.username && senha == admin.senha){
-        localStorage.setItem("Usuario", login);
-        //window.location.href = "www.google.com.br";
-        window.location.assign("http://pt.stackoverflow.com");
-    }
-    else{
-        
-  } */
+    
 }
