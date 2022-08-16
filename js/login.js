0c9cec8dddfeac720646593a8a3f9e4d072b65b0
Ricardo Lisboa <ricardo.lisboa17@hotmail.com>
2022-08-15 23:24:38 -0300

    alteracoes

--- js/login.js
+++ js/login.js
@@ -4,17 +4,36 @@ var admin = {
     password: "1234"
 }
 
+var maria = {
+    username: "maria",
+    password: "1234"
+}
+
+var jose = {
+    username: "jose",
+    password: "1234"
+}
+
 function Entrar(){
 
-    var login = document.getElementById("login").value
-    var senha = document.getElementById("password").value 
+    var login = document.getElementById("login")
+    var senha = document.getElementById("password")
 
-    if(login == admin.username && senha == admin.password){
+    if(login.value == "" || senha.value == "" ){
+        alert("preencha os campos");
+        login.focus();
+        return
+    }
+
+    if(login.value == admin.username && senha.value == admin.password){
         alert("Logado com sucesso!");
         localStorage.setItem("Usuario", login);
         window.location.href = "home.html"; 
     }else{
-        alert("Login ou senha incorretos!");
+        const element = document.getElementById('erros');
+        element.classList.remove('d-none');
+        login.value = ""
+        senha.value = ""
+        login.focus();
     }
-    
 }
